32d86a1f9b16fb7c4cae14dd68d4d787620e5ff6
Manishkumarprajapat <73461155+Manishkumarprajapat@users.noreply.github.com>
2021-02-22 06:33:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var balloon;
+var database;
+var ref;
+
+function preload() {
+  hotballoon=loadImage("Hot Air Ballon-01.png")
+ balloonmove=loadAnimation("Hot Air Ballon-02.png","Hot Air Ballon-03.png","Hot Air Ballon-04.png")
+}
+function setup() {
+  createCanvas(1500,600);
+  
+  ball= createSprite(100,50,30,30)
+  ball.addAnimation("moveballoon",balloonmove)
+  var balloonPosition=database.ref('balloon/height')
+  balloonPosition.on("value",readPosition,showError)
+}
+
+function draw() {
+  background(hotballoon);
+  
+  ball.display();
+  if(keyDown(LEFT_ARROW)){
+    ball.x=ball.x-10
+  }  
+  if(keyDown(RIGHT_ARROW)){
+    ball.x=ball.x+10
+  } 
+  if(keyDown(UP_ARROW)){
+    ball.y=ball.y-10
+  } 
+  if(keyDown(DOWN_ARROW)){
+    ball.y=ball.y+10
+  } 
+
+  drawSprites();
+}
